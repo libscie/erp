8365d9c721f890c2d8c68182f9f39ff8c51cba1d
Chris Hartgerink <chris@libscie.org>
2023-06-08 13:38:28 +0200

    Remove CSS, improve login

--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -3,6 +3,7 @@ import { api } from "src/blitz-server"
 import db from "db"
 // https://www.passportjs.org/packages/passport-github2/
 import passportGithub2 from "passport-github2"
+import { Role } from "types"
 
 export default api(
   passportAuth(({ ctx, req, res }) => ({
@@ -14,12 +15,37 @@ export default api(
         // https://github.com/libscie/ResearchEquals.com/blob/main/pages/api/auth/%5B...auth%5D.ts
         strategy: new passportGithub2(
           {
-            clientID: "3e4e9e61f94a55e5ca05",
-            clientSecret: "c9b16215f45f816300176519f28a4c58ee573e39",
-            callbackURL: "http://localhost:3000/api/auth/github/callback",
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: `${process.env.APP_ORIGIN}/api/auth/github/callback`,
           },
-          function (accessToken, refreshToken, profile, done) {
+          async function (accessToken, refreshToken, profile, done) {
             console.log(profile)
+
+            let user
+            try {
+              user = await db.user.findFirstOrThrow({
+                where: {
+                  email: profile._json.email,
+                },
+              })
+            } catch (e) {
+              console.log("Creating new user...")
+              user = await db.user.create({
+                data: {
+                  name: profile.displayName,
+                  email: profile._json.email,
+                  avatar: profile._json.avatar_url,
+                },
+              })
+            }
+
+            return done(null, {
+              publicData: {
+                userId: user.id,
+                role: user.role as Role,
+              },
+            })
           }
         ), // Provide initialized passport strategy here
       },
