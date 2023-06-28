ddf3ba277f2272fd2c2484e45b864343524d4043
Chris Hartgerink <chris@libscie.org>
2023-06-28 11:30:17 +0200

    Update header links

--- src/core/components/NavHeader.tsx
+++ src/core/components/NavHeader.tsx
@@ -6,17 +6,12 @@ import {
   Header,
   HeaderName,
   HeaderGlobalBar,
-  HeaderGlobalAction,
-  HeaderPanel,
-  SwitcherItem,
-  SwitcherDivider,
   HeaderMenuItem,
   HeaderMenu,
   HeaderNavigation,
 } from "carbon-components-react"
 import Link from "next/link"
-import { Suspense, useState } from "react"
-import { Search, Notification, Switcher } from "@carbon/react/icons"
+import { Suspense } from "react"
 
 const LogInOut = () => {
   const currentUser = useCurrentUser()
@@ -46,81 +41,22 @@ const LogInOut = () => {
 }
 
 const NavHeader = () => {
-  const [notifications, openNotifications] = useState(false)
-
   return (
     <Header aria-label="LibScie Manager">
       <HeaderName href="#" prefix="LibScie">
         Manager
       </HeaderName>
-      <HeaderNavigation aria-label="IBM [Platform]">
-        <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
-        <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-        <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-        <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
-          <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
-          <HeaderMenuItem isActive href="#">
-            Sub-link 2
-          </HeaderMenuItem>
-          <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+      <HeaderNavigation aria-label="Navigation links">
+        <HeaderMenu aria-label="Quick Links" menuLinkName="Quick Links">
+          <HeaderMenuItem href="https://libscie.org">Blog</HeaderMenuItem>
+          <HeaderMenuItem href="https://libscie.org/ghost">Blog editor</HeaderMenuItem>
+          <HeaderMenuItem href="https://handbook.libscie.org/">Handbook</HeaderMenuItem>
         </HeaderMenu>
       </HeaderNavigation>
       <HeaderGlobalBar>
         <Suspense fallback="Loading...">
           <LogInOut />
         </Suspense>
-        <HeaderGlobalAction
-          aria-label="Search"
-          onClick={() => {
-            alert("search click")
-          }}
-        >
-          <Search size={20} />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction
-          aria-label="Notifications"
-          isActive={notifications}
-          onClick={() => {
-            openNotifications(!notifications)
-          }}
-        >
-          <Notification size={20} />
-          {/* <HeaderPanel
-              expanded={notifications}
-              onHeaderPanelFocus={notifications}
-              href="#switcher-button"
-            >
-              <SwitcherItem aria-label="Link 1" href="#">
-                Link 1
-              </SwitcherItem>
-              <SwitcherDivider />
-              <SwitcherItem href="#" aria-label="Link 2">
-                Link 2
-              </SwitcherItem>
-              <SwitcherItem href="#" aria-label="Link 3">
-                Link 3
-              </SwitcherItem>
-              <SwitcherItem href="#" aria-label="Link 4">
-                Link 4
-              </SwitcherItem>
-              <SwitcherItem href="#" aria-label="Link 5">
-                Link 5
-              </SwitcherItem>
-              <SwitcherDivider />
-              <SwitcherItem href="#" aria-label="Link 6">
-                Link 6
-              </SwitcherItem>
-            </HeaderPanel> */}
-        </HeaderGlobalAction>
-        <HeaderGlobalAction
-          aria-label="App Switcher"
-          onClick={() => {
-            alert("app-switcher click")
-          }}
-          tooltipAlignment="end"
-        >
-          <Switcher size={20} />
-        </HeaderGlobalAction>
       </HeaderGlobalBar>
     </Header>
   )
