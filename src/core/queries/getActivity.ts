d11ac6697b31d87a780a00a6354022e952499fe1
Chris Hartgerink <chris@libscie.org>
2023-07-04 23:34:56 +0200

    Add skeleton for /activity/[id]

---
+++ src/core/queries/getActivity.ts
@@ -0,0 +1,12 @@
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+
+export default resolver.pipe(async (id: number) => {
+  const activities = await db.activity.findFirstOrThrow({
+    where: {
+      id,
+    },
+  })
+
+  return activities
+})
---
+++ src/pages/activity/[id].tsx
@@ -0,0 +1,36 @@
+import { BlitzPage } from "@blitzjs/next"
+import { gSSP } from "src/blitz-server"
+import getActivity from "../../core/queries/getActivity"
+import Layout from "src/core/layouts/Layout"
+
+export const getServerSideProps = gSSP(async ({ params }) => {
+  const activity = await getActivity(parseInt(params!.id as any), undefined as any)
+
+  return { props: { activity } }
+})
+
+const ActivityViewer: BlitzPage = (activity) => {
+  return (
+    <Layout title="Home">
+      <main>
+        <div className="cds--grid marginal">
+          <div className="cds--row">
+            <div className="cds--col">
+              <div className="outside">
+                <div className="inside">
+                  {/* <Suspense fallback="Loading...">
+                    <AddActivity />
+                  </Suspense> */}
+                  {JSON.stringify(activity)}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer></footer>
+    </Layout>
+  )
+}
+
+export default ActivityViewer
