a1af4b8062574d83d21cd0af1c1e4f6c71d79cd8
Chris Hartgerink <chris@libscie.org>
2023-07-26 15:12:07 +0200

    Add document model

---
+++ db/migrations/20230726131154_add_document/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "contents" JSONB NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
