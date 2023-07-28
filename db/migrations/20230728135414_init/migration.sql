98aebf5a3b69db707ec5ede96aadbf7696412b50
Chris Hartgerink <chris@libscie.org>
2023-07-28 17:37:36 +0200

    Update migrations to clean db

--- db/migrations/20230728135414_init/migration.sql
+++ db/migrations/20230728135414_init/migration.sql
@@ -75,6 +75,27 @@ CREATE TABLE "Document" (
     CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Budget" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "lineItems" TEXT[],
+    "lineValues" DECIMAL(65,30)[],
+    "lineEmissions" DECIMAL(65,30)[],
+
+    CONSTRAINT "Budget_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_BudgetToBudget" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_handle_key" ON "User"("handle");
 
@@ -83,6 +104,12 @@ CREATE UNIQUE INDEX "Session_handle_key" ON "Session"("handle");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Token_hashedToken_type_key" ON "Token"("hashedToken", "type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_BudgetToBudget_AB_unique" ON "_BudgetToBudget"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_BudgetToBudget_B_index" ON "_BudgetToBudget"("B");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -95,3 +122,9 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BudgetToBudget" ADD CONSTRAINT "_BudgetToBudget_A_fkey" FOREIGN KEY ("A") REFERENCES "Budget"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BudgetToBudget" ADD CONSTRAINT "_BudgetToBudget_B_fkey" FOREIGN KEY ("B") REFERENCES "Budget"("id") ON DELETE CASCADE ON UPDATE CASCADE;
